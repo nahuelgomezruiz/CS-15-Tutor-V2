11ed42d13757a90ef3e80a001678648d44a8f441
nahuelgomezruiz <alfredo.gomez_ruiz@tufts.edu>
2026-02-06 13:14:57 -0500

    Changed tab title

--- frontend/web-app/pages/_document.tsx
+++ frontend/web-app/pages/_document.tsx
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        
+        {/* Meta tags */}
+        <meta name="description" content="CS 15 Data Structures AI Tutor - Tufts University" />
+        <meta name="theme-color" content="#3B82F6" />
+        
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
